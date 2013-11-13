e682d412012219e3a43154cbd97c74c2a41c51d3
Alan Ablett <alan.ablett1@googlemail.com>
2013-11-13 23:08:06 +0000

    Added clearer notes and tidied up

--- public/desktop.js
+++ public/desktop.js
@@ -6,21 +6,21 @@ window.onload = function() {
     // Let the server know we're connected
     socket.emit('desktop.connect');
     
-    // Once connected we listen for the server to call back with out unique string (this'll be used by the mobile)
+    // Once connected we listen for the server to call back with our unique string (this'll be used by the mobile)
     socket.on('desktop.callback', function( data ){
         content.innerHTML = data.url;
     });
     
-    // When our mobile pushes data up the server will fire off a desktop move call to the appropriate socket.io client
-    // with the relevant data whether we're going up or down
+    // When our mobile pushes data up the server it will fire off a desktop move call to the appropriate socket.io client
+    // with the relevant data whether we're going up or down, we scroll the window in that direction
     socket.on('desktop.move', function( data ){
         var top = $(window).scrollTop();
         
         if( data.direction == 'up' ){
-            $("html, body").animate({ scrollTop: top-100 }, 500);
+            $("html, body").animate({ scrollTop: top-200 }, 500);
         }
         if( data.direction == 'down' ){
-            $("html, body").animate({ scrollTop: top+100 }, 500);
+            $("html, body").animate({ scrollTop: top+200 }, 500);
         }
     });
 
--- server.js
+++ server.js
@@ -3,46 +3,44 @@ var express = require("express");
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 
+// Bootstrap the essentials
 app.use(express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/templates');
-
 server.listen(8082);
 
-// Desktop
+// Home view (show the url associated with the browsers socket.io id)
 app.get('/', function (req, res) {
 	res.render("desktop.html");
 });
 
-var desktopId;
-var url;
-var connections = [];
-
-io.sockets.on('connection', function (socket) {
-	
-	socket.on('desktop.connect', function (data) {
-		url = Math.random().toString(36).substring(10);
-		connections[url] = socket.id;
-		socket.emit('desktop.callback', { url : url });
-	});
-	
-	socket.on('mobile.control', function(data){
-		url = data.url;
-		id = connections[url];
-		io.sockets.socket(id).emit('desktop.move', { direction : data.direction });
-	});
-	
-	socket.on('mobile.connect', function (data) {
-		
-	});
-});
-
+// All other requests get the mobile view
 app.get('*', function(req,res) {
 	url = req.url.substring(1);
 	res.render("mobile.html");
 });
 
-//Mobile
-// app.get('/', function (req, res) {
-// 	res.render("mobile.html");
-// });
+// Store connections in an array that links socket.io id's to "url's"
+var connections = [];
+
+io.sockets.on('connection', function (socket) {
+	
+	// When a desktop client connects generate a random string for a url. We'll assign
+	// this the socket id so we know which client our mobile user should control. We pass
+	// this url to the desktop using desktop.callback so it can be displayed on screen
+	socket.on('desktop.connect', function (data) {
+		url = Math.random().toString(36).substr(2, 5);
+		connections[url] = socket.id;
+		socket.emit('desktop.callback', { url : url });
+	});
+	
+	// When the mobile calls the mobile.control function, we are passed the url that the mobile
+	// is on, and the direction we want the client to move in. We check for the clients
+	// socket.io id based on the url, then pass the direction to that client. The client listens
+	// for this call and will scroll the window in the correct direction
+	socket.on('mobile.control', function(data){
+		url = data.url;
+		id = connections[url];
+		io.sockets.socket(id).emit('desktop.move', { direction : data.direction });
+	});
+});
